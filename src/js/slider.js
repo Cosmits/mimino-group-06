3d5f5a38809f6d9a071afb89da3bfc97637389d2
Marynaoop <marynaruduk@icloud.com>
2023-02-18 15:04:54 +0200

    push

--- src/js/slider.js
+++ src/js/slider.js
@@ -5,4 +5,27 @@
       prevEl: ".swiper-button-prev",
     },
 
-  });
+    autoplay: {
+        delay: 3000,
+      },
+      loop: true,
+
+      breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20
+        },
+        // when window width is >= 480px
+        480: {
+          slidesPerView: 1,
+          spaceBetween: 30
+        },
+        // when window width is >= 640px
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 40
+        }
+      }
+  }
+  );
