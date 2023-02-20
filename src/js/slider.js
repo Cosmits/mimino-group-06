32d050a0eb5847e74545970bf11a27995ba04450
Cosmits <salge.cos@gmail.com>
2023-02-20 19:45:36 +0100

    add slider to home page

--- src/js/slider.js
+++ src/js/slider.js
@@ -27,4 +27,32 @@
         }
       }
   }
-  );
+);
+  
+
+var swiper = new Swiper(".mySwiper_2", {
+      slidesPerView: 1,
+      spaceBetween: 25,
+      freeMode: true,
+       navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+     },
+     breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+       },
+        // when window width is >= 414px
+       414: {
+      slidesPerView: 2,
+      spaceBetween: 20
+       },
+    // when window width is >= 480px
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    }
+  }
+    });
