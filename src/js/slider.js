570090090720917c6aeefaec3922969764dc2f25
Gabriella Marusyak <slipuhinag@gmail.com>
2023-02-21 04:45:04 +0200

    Create swipers

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,58 +1,97 @@
 
- var swiper = new Swiper(".mySwiper", {
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    autoplay: {
-        delay: 3000,
-      },
-      loop: true,
+//  var swiper = new Swiper(".mySwiper", {
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//     autoplay: {
+//         delay: 3000,
+//       },
+//       loop: true,
 
-      breakpoints: {
-        // when window width is >= 414px
-        320: {
-          slidesPerView: 1,
+//       breakpoints: {
+//         // when window width is >= 414px
+//         320: {
+//           slidesPerView: 1,
        
-        },
-        // when window width is >= 1024px
-        480: {
-          slidesPerView: 1,
+//         },
+//         // when window width is >= 1024px
+//         480: {
+//           slidesPerView: 1,
      
-        },
-        // when window width is >= 1280px
-        640: {
-          slidesPerView: 1,
+//         },
+//         // when window width is >= 1280px
+//         640: {
+//           slidesPerView: 1,
 
-        }
-      }
-  }
-);
+//         }
+//       }
+//   }
+// );
   
 
-var swiper = new Swiper(".mySwiper_2", {
-      slidesPerView: 1,
+// var swiper = new Swiper(".mySwiper_2", {
+//       slidesPerView: 1,
+//       spaceBetween: 25,
+//       freeMode: true,
+//        navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//      },
+//      breakpoints: {
+//     // when window width is >= 320px
+//     320: {
+//       slidesPerView: 1,
+//       spaceBetween: 20
+//        },
+//         // when window width is >= 414px
+//        414: {
+//       slidesPerView: 2,
+//       spaceBetween: 20
+//        },
+//     // when window width is >= 480px
+//     1024: {
+//       slidesPerView: 2,
+//       spaceBetween: 30
+//     }
+//   }
+//     });
+
+
+    var swiper = new Swiper(".mySwiper", {
+       slidesPerView: 1,
       spaceBetween: 25,
+    allowSlideNext: true,
       freeMode: true,
        navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
-     },
-     breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 20
-       },
-        // when window width is >= 414px
-       414: {
+      },
+      breakpoints: {
+     // when window width is >= 320px
+     320: {
+       slidesPerView: 1,
+       spaceBetween: 25
+        },
+         // when window width is >= 414px
+          414: {
       slidesPerView: 2,
-      spaceBetween: 20
-       },
-    // when window width is >= 480px
-    1024: {
+       spaceBetween: 40
+        },
+        // when window width is >= 930px
+      
+       1024: {
       slidesPerView: 2,
-      spaceBetween: 30
-    }
-  }
-    });
+       spaceBetween: 40
+        },
+      },
+    //   autoplay: {
+    //    delay: 3000,
+    //  },
+      loop: true,
+      slideToClickedSlide: true,
+    
+      // centeredSlides: true,
+  
+   });
+
