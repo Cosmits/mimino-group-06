c27ca62f7465a7909237f78723336cc9ac404f1c
Yan <yan-it@ukr.net>
2023-02-22 22:28:07 +0200

    updated breakpoints to our project maket

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,81 +1,31 @@
 (() => {
-
-  var swiper = new Swiper(".mySwiper_room", {
+  var swiper = new Swiper('.mySwiper_room', {
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
     autoplay: {
-        delay: 3000,
+      delay: 3000,
+    },
+    loop: true,
+
+    breakpoints: {
+      // when window width is >= 414px
+      320: {
+        slidesPerView: 1,
       },
-      loop: true,
-
-      breakpoints: {
-        // when window width is >= 414px
-        320: {
-          slidesPerView: 1,
-       
-        },
-        // when window width is >= 1024px
-        480: {
-          slidesPerView: 1,
-     
-        },
-        // when window width is >= 1280px
-        640: {
-          slidesPerView: 1,
-
-        }
-      }
-  }
-);
-  
-
-  var swiper = new Swiper(".mySwiper", {
-      slidesPerView: 'auto',
-      spaceBetween: 40,
-    // allowSlideNext: true,
-      freeMode: true,
-      speed: 1500,
-
-      autoplay: {
-        delay: 2000,
-      },   
-
-       navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+      // when window width is >= 1024px
+      480: {
+        slidesPerView: 1,
       },
-      breakpoints: {
-     // when window width is >= 320px
-     320: {
-       slidesPerView: 1,
-       spaceBetween: 25
-        },
-         // when window width is >= 414px
-          414: {
-      slidesPerView: 1,
-       spaceBetween: 40
-        },
-        // when window width is >= 930px
-      
-       930: {
-      slidesPerView: 2,
-       spaceBetween: 26
-       },
+      // when window width is >= 1280px
+      640: {
+        slidesPerView: 1,
       },
-    //   autoplay: {
-    // delay: 3000,
-    //   },
-      loop: true,
-      slideToClickedSlide: true,
-    
-      // centeredSlides: true,
-  
+    },
   });
-  
 
-  var swiper = new Swiper(".mySwiper_2", {
+  var swiper = new Swiper('.mySwiper', {
     slidesPerView: 'auto',
     spaceBetween: 40,
     // allowSlideNext: true,
@@ -87,25 +37,25 @@
     },
 
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
     breakpoints: {
       // when window width is >= 320px
       320: {
         slidesPerView: 1,
-        spaceBetween: 25
+        spaceBetween: 25,
       },
       // when window width is >= 414px
       414: {
         slidesPerView: 1,
-        spaceBetween: 40
+        spaceBetween: 40,
       },
       // when window width is >= 930px
 
       930: {
         slidesPerView: 2,
-        spaceBetween: 26
+        spaceBetween: 26,
       },
     },
     //   autoplay: {
@@ -115,8 +65,47 @@
     slideToClickedSlide: true,
 
     // centeredSlides: true,
-
   });
 
+  var swiper = new Swiper('.mySwiper_2', {
+    slidesPerView: 'auto',
+    spaceBetween: 40,
+    // allowSlideNext: true,
+    freeMode: true,
+    speed: 1500,
 
+    autoplay: {
+      delay: 2000,
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      // when window width is >= 414px
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 34,
+      },
+      // when window width is >= 930px
+
+      1280: {
+        slidesPerView: 2,
+        spaceBetween: 86,
+      },
+    },
+    //   autoplay: {
+    // delay: 3000,
+    //   },
+    loop: true,
+    slideToClickedSlide: true,
+
+    // centeredSlides: true,
+  });
 })();
