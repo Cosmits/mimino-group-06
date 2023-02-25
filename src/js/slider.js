76055d444c22f8254cf012ac3c8db6e005d793e7
Illia Shatokhin <resonance.2106@gmail.com>
2023-02-25 23:31:32 +0200

    Повністю працююча секція 1 зі слайдером

--- src/js/slider.js
+++ src/js/slider.js
@@ -89,3 +89,27 @@
 
   });
 })();
+
+var swiper = new Swiper('.mySwiper_0', {
+  slidesPerView: 'auto',
+  spaceBetween: 25,
+  freeMode: true,
+  speed: 500,
+
+  // autoplay: {
+  //   delay: 2000,
+  // },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+  },
+  loop: true,
+  slideToClickedSlide: true,
+});
